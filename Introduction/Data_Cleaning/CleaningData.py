f2fa3870eb44995a53869259b802dfccdd51a25c
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-01-29 11:53:53 +0100

    First commit with Data Cleaning

---
+++ Introduction/Data_Cleaning/CleaningData.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+crime = pd.read_csv('../Data/crime_boston.csv')
+
+# Access rows that contain a null
+print(crime.isnull().any(axis=1))
+
+# Extract the indices of the rows with null values
+# and use them to get the rows and save them as result
+rows_with_missing_values = crime.isnull().any(axis=1)
+result = crime[rows_with_missing_values]
+
+# These are the rows with NaN values in them.
+print(result)
+
+# Now lets try to drop the year, month and hour columns
+# since the occurred on date column already includes both date and time
+cleaned_crime = crime.drop(columns=['YEAR', 'MONTH', 'HOUR'])
+
+# Now lets check the DataFrame for misspellings.
+# Let's start with 'OFFENSE_CODE_GROUP
+print(cleaned_crime['OFFENSE_CODE_GROUP'].unique())
+# THere seem to be no misspellings here.
+
+# Now we are checking 'OFFENSE_DESCRIPTION
+print(cleaned_crime['OFFENSE_DESCRIPTION'].unique())
+# No misspellings seems to be present
+
+# Now we check 'DAY_OF_WEEK'
+print(cleaned_crime['DAY_OF_WEEK'].unique())
+# No misspellings here.
---
+++ Introduction/Data_Cleaning/Notes_Data_Cleaning.md
@@ -0,0 +1,19 @@
+# Data Cleaning
+- Missing values
+- Formatting
+- Structure
+- Complex values
+- Unit conversion
+- Interpretation of magnitudes
+
+Data cleaning examples:
+- Misspellings
+- Duplicated rows
+- Inconsistent formats
+- Unspecified units
+
+Data Cleaning Questions:
+- Are there missing values in the dataset?
+- Are there missing values that were filled in?
+- Which parts of the data were entered by humans?
+
